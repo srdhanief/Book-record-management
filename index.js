062fe5040dc440fbd986f7d32b557cd70e6e3bfd
srdhanief <srdhanief@gmail.com>
2023-01-30 23:49:00 +0530

    fullycompleted

---
+++ index.js
@@ -0,0 +1,80 @@
+/*const express = require("express");
+
+const DbConnection = require("./databaseConnection");
+
+const dotenv = require("dotenv");
+ 
+const usersRouter = require("./routes/users");
+const booksRouter = require("./routes/books");
+
+dotenv.confiq();
+const app = express();
+ 
+DbConnection();
+
+const PORT = 8081;
+
+app.use(express.json());
+
+
+
+
+
+app.get("/",function(req, res){
+    res.send("Hello World");
+});
+
+
+app.use("/users", usersRouter);
+app.use("/books", booksRouter); 
+
+
+
+
+app.listen("3000", function(){
+    console.log("Port is started at 3000");
+});*/
+
+const express = require("express");
+
+// import DB connection file
+const DbConnection = require("./databaseConnection");
+
+// import db 
+const dotenv = require("dotenv");
+
+
+// import routes
+const usersRouter = require("./routes/users");
+const booksRouter = require("./routes/books");
+
+dotenv.config();
+
+const app = express();
+
+DbConnection();
+
+const PORT = 8081;
+
+app.use(express.json());
+
+// npm i nodemon --save-dev
+// const data = ["rohan", "dev"];
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server is up and running succesfully",
+  });
+});
+
+// http://localhost:801/users/
+app.use("/users", usersRouter);
+app.use("/books", booksRouter);
+
+app.get("*", (req, res) => {
+  res.status(404).json({
+    message: "This route doesn't exist",
+  });
+});
+app.listen(3000, () => {
+  console.log("Server is running at port 3000");
+});
